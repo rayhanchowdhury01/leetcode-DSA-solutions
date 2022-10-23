cb64be369de8381b57680330d6d10a10791c1e9a
rayhanchowdhury01 <70866390+rayhanchowdhury01@users.noreply.github.com>
2022-10-24 00:17:49 +0600

    Time: 6 ms (36.57%), Space: 42.9 MB (5.37%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,11 +1,7 @@
 class Solution {
-     public boolean checkIfPangram(String sentence) {
-        int[] freq = new int[26];
-        char[] ch = sentence.toCharArray();
-        for (char c : ch) freq[c - 'a']++;
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] == 0) return false;
-        }
-        return true;
+       public boolean checkIfPangram(String sentence) {
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < sentence.length(); i++) set.add(sentence.charAt(i));
+        return set.size() == 26;
     }
 }
