3465a032d45834eccadad1b34af1801b2b4772a6
rayhanchowdhury01 <70866390+rayhanchowdhury01@users.noreply.github.com>
2022-12-14 04:50:14 +0600

    Time: 1 ms (91.76%), Space: 43.2 MB (85.71%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,72 +1,30 @@
 
 class Solution {
-    public boolean isBipartite(int[][] graph) {
-        Dsu dsu = new Dsu(graph.length);
-        for (int i = 0; i < graph.length; i++) {
-            for (int j = 0; j < graph[i].length; j++) {
-                if (dsu.isSame(i, graph[i][j])) return false;
-                dsu.unionSets(graph[i][0], graph[i][j]);
+    static int[] color;
+    public boolean isBipartite(int[][] g) {
+        color = new int[g.length];
+        return Solve(g);
+    }
+    public static boolean Solve(int[][] g) {
+        for (int i = 0; i < g.length; i++) {
+            if (color[i] == 0) {
+                color[i] = 1;
+                Queue<Integer> que = new ArrayDeque<>();
+                que.add(i);
+                while (!que.isEmpty()) {
+                    int u = que.poll();
+                    for (int v : g[u]) {
+                        if (color[v] == color[u]) return false;
+                        if (color[v] == 0) {
+                            que.add(v);
+                            color[v] = -color[u] ;
+                        }
+                    }
+                }
+
             }
         }
         return true;
     }
 
-    public static class Dsu {
-        int n, numberOfset;
-        int[] parent, setSize, rank;
-
-        Dsu(int n) {
-            this.n = n;
-            numberOfset = n;
-            parent = new int[n];
-            rank = new int[n];
-            setSize = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                setSize[i] = 1;
-            }
-        }
-
-        public int findSet(int u) {
-            if (u == parent[u]) return u;
-            return parent[u] = findSet(parent[u]);
-        }
-
-        public boolean union(int u, int v) {
-            u = findSet(u);
-            v = findSet(v);
-            if (u == v) return false;
-            if (setSize[u] <= setSize[v]) {
-                int temp = u;
-                u = v;
-                v = temp;
-            }
-            parent[v] = u;
-            setSize[u] += setSize[v];
-            numberOfset--;
-            return true;
-        }
-
-        public void unionSets(int a, int b) {//union by rank
-            a = findSet(a);
-            b = findSet(b);
-            if (a != b) {
-                if (rank[a] < rank[b]) {
-                    int t = a;
-                    a = b;
-                    b = t;
-                }
-                parent[b] = a;
-                if (rank[a] == rank[b]) {
-                    rank[a]++;
-                }
-            }
-        }
-
-        public boolean isSame(int u, int v) {
-            return findSet(u) == findSet(v);
-        }
-    }
-
 }
-
