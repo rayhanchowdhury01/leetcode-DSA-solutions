8776bf5f9fa3843df86a8764c213c35556907fd7
rayhanchowdhury01 <70866390+rayhanchowdhury01@users.noreply.github.com>
2023-02-25 13:20:56 +0600

    Time: 1 ms (100.00%), Space: 42.4 MB (16.92%) - LeetHub

--- 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
+++ 1394-find-lucky-integer-in-an-array/1394-find-lucky-integer-in-an-array.java
@@ -2,9 +2,9 @@ class Solution {
     public int findLucky(int[] arr) {
         
         int[] frequency = new int[501];
-        for(int i = 0;i < arr.length; ++i){
+        for(int i = 0;i < arr.length; ++i)
             frequency[arr[i]]++; 
-        }
+        
        
         for(int i = 500;i >=1 ; --i)
             if(frequency[i] == i) return i;
