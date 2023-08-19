0d6c615d7ad95022770cc551549c20e276b6882b
Rayhan Chowdhury <70866390+rayhanchowdhury01@users.noreply.github.com>
2023-08-19 22:23:40 +0600

    Time: 7 ms (100.00%), Space: 44.6 MB (100.00%) - LeetHub

--- 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.java
@@ -1,28 +1,22 @@
 class Solution {
-    public boolean canMakeSubsequence(String str1, String str2) {
-        if (str2.length() > str1.length()) return false;
+			public boolean canMakeSubsequence(String str1, String str2) {
+				if (str2.length() > str1.length()) return false;
+
+				int n = str1.length();
+				int m = str2.length();
+				int x = 0, i = n - 1, j = m - 1;
+				
+				while (i >= 0 && j >= 0) {
+					if (str2.charAt(j) == str1.charAt(i)) {
+						x++; i--; j--;
+					} else if (str2.charAt(j) == 'a' && str1.charAt(i) == 'z') {
+						x++; i--; j--;
+					} else if ((int) str1.charAt(i) - 'a' + 1 == (int) str2.charAt(j) - 'a') {
+						x++; i--; j--;
+					} else i--;
+				}
+				return x == str2.length();
 
 
-        int n = str1.length();
-        int m = str2.length();
-        int x = 0, i = n - 1, j = m - 1;
-        while (i >= 0 && j >= 0) {
-            if (str2.charAt(j) == str1.charAt(i)) {
-                x++;
-                i--;
-                j--;
-            } else if (str2.charAt(j) == 'a' && str1.charAt(i) == 'z') {
-                x++;
-                j--;
-                i--;
-            } else if ((int) str1.charAt(i) - 'a' + 1 == (int) str2.charAt(j) - 'a'){
-                i--;
-                j--;
-                x++;
-            } else i--;
-        }
-        return x == str2.length();
-
-
-    }
-}
+			}
+		}
