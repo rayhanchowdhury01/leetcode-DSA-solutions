73251d3d6ced7e7b395ff67b5bee9fd2389bbc03
Rayhan Chowdhury <70866390+rayhanchowdhury01@users.noreply.github.com>
2023-08-25 20:43:54 +0600

    Time: 18 ms (69.82%), Space: 45.1 MB (89.24%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -14,21 +14,40 @@
  * }
  */
 class Solution {
-    PriorityQueue<Integer> pq = new PriorityQueue<>();
-    
+     
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
         List<Integer> res = new ArrayList<>();
-        Inorder(root1);
-        Inorder(root2);
-        while (!pq.isEmpty()) res.add(pq.poll());
+        Inorder(root1, l1);
+        Inorder(root2, l2);
+        int i = 0, j = 0;
+        while (i < l1.size() && j < l2.size()) {
+            if (l1.get(i) < l2.get(j)) {
+                res.add(l1.get(i));
+                i++;
+            } else if (l2.get(j) < l1.get(i)) {
+                res.add(l2.get(j));
+                j++;
+            } else {
+                res.add(l2.get(j));
+                res.add(l2.get(j));
+                i++;
+                j++;
+            }
+
+        }
+        while (i < l1.size()) res.add(l1.get(i++));
+        while (j < l2.size()) res.add(l2.get(j++));
         return res;
     }
-
-    public void Inorder(TreeNode tree) {
+    
+    public static void Inorder(TreeNode tree, List<Integer> list) {
         if (tree != null) {
-            Inorder(tree.left);
-            pq.offer(tree.val);
-            Inorder(tree.right);
+            Inorder(tree.left, list);
+            list.add(tree.val);
+            Inorder(tree.right, list);
         }
     }
 }
