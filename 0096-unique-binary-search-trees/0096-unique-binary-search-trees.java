1aeba6af71f809fa9dd6ed380d5b9ad1beda1120
Rayhan Chowdhury <70866390+rayhanchowdhury01@users.noreply.github.com>
2023-08-25 16:02:57 +0600

    Time: 2203 ms (5.71%), Space: 39.4 MB (35.55%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,6 +1,6 @@
 class Solution {
     
-     public int numTrees(int n) {
+    /* public int numTrees(int n) {
         return catalan(n);
     }
 
@@ -8,6 +8,18 @@ class Solution {
         int res = 0;
         if (n <= 1) return 1;
         for (int i = 0; i < n; i++) res += catalan(i) * catalan(n - (i + 1));
+        return res;
+    }*/
+    
+     public int numTrees(int n) {
+        return Tree(1, n);
+    }
+
+    public int Tree(int l, int r) {
+        if (r <= l) return 1;
+        int res = 0;
+        for (int i = l; i <= r; i++) res += Tree(l, i - 1) * Tree(i + 1, r);
+
         return res;
     }
 }
